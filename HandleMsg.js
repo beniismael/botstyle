be25bb45aafd691588bf086015457114b764e005
beniismael <74507319+beniismael@users.noreply.github.com>
2020-11-29 09:01:13 +0700

    Update HandleMsg.js

--- HandleMsg.js
+++ HandleMsg.js
@@ -447,7 +447,7 @@ module.exports = HandleMsg = async (aruga, message) => {
         case 'rate':
             if (!isGroupMsg) return aruga.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
             const seberapagay = args.join(' ')
-            const awr = seberapagay[Math.floor(Math.random() * (seberapagay.length))]
+            const awr = rate[Math.floor(Math.random() * (rate.length))]
             if (!rating) aruga.reply(from, '⚠️ Format salah! Ketik *#menu* untuk penggunaan.')
             await aruga.sendText(from, `Pertanyaan: *${rating}* \n\nJawaban: ${awr}`)
             break
